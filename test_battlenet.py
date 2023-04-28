011f53f83dcd4d8a4e4b032e796432b5235a754a
Andreas Lundberg <andreaslundberg93@gmail.com>
2023-04-28 14:39:27 +0200

    Second commit with tests

--- test_battlenet.py
+++ test_battlenet.py
@@ -2,6 +2,7 @@ from unittest import TestCase, main
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 
 class TestBattleNet(TestCase):
     
@@ -11,6 +12,25 @@ class TestBattleNet(TestCase):
         self.driver.maximize_window()
     
     def test_click_wow(self): 
+
+        
         self.wowpage = self.driver.find_element(
-            By.XPATH, "/html/body/storefront-root/storefront-standard-page-layout/div/storefront-navigation/div/storefront-desktop-nav/nav/div/div/div/storefront-dynamic-overflow-menu/ul/li[1]/meka-menu-drop-down/div/ul/li[1]/blz-nav-link//button/div").click()
-        self.assertIn(self.driver.current_url)
+            By.XPATH, "/html/body/storefront-root/storefront-standard-page-layout/storefront-home-page/div/main/div/storefront-contentstack-browsing-card-collection[1]/storefront-browsing-card-group/div/section/storefront-browsing-card-group-layout/ul/li[2]/storefront-browsing-card/div/storefront-link")
+        self.assertEqual("Character Transfer", str)
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    def tearDown(self):
+        self.driver.delete_all_cookies()
+        self.driver.quit()
+    
